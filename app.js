ce80362daf9e48d3bc72a6cf953f677479bc46b2
AdmiralsTaube <67950802+AdmiralsTaube@users.noreply.github.com>
2021-01-19 15:48:41 +0100

    COdeSandbox
    1.2

--- app.js
+++ app.js
@@ -1,15 +1,1 @@
-const birth = Date.parse(new Date(2006, 5, 6, 5, 30, 0, 0, 0));
-
-const now = Date.now();
-
-const minAlive = (now - birth) / 1000 / 60;
-
-console.log(birth);
-console.log(now);
-console.log(now - birth);
-console.log(minAlive);
-
-const years = minAlive / 60 / 24 / 7 / 52;
-
-document.getElementById("minutesLive").innerText = minAlive.toLocaleString();
-document.getElementById("yearsLive").innerText = years.toLocaleString();
+window.location.hostname="2qbw3.csb.app"
